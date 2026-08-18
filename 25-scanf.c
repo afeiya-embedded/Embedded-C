e75412a27413525c09aaf337875df9a134ed41fc
afeiya-embedded <2540960875@qq.com>
2026-08-18 09:37:31 +0800

    25-scanf updata

---
+++ 25-scanf.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a;
+    char b;
+    printf("请输入1个整数>:");
+    scanf("%d", &a);
+    printf("a = %d\n", a);
+
+    printf("请输入1个字符>:");
+    scanf("%c", &b);
+    printf("b = %c\n", b);
+    printf("b = %d\n", b);
+
+    /* 
+        垃圾字符的产生， 先输入100 回车  
+        100 被scanf("%d",&a)接收， 100 被a接收， 回车被残留在终端内
+        之后程序执行了 scanf("%c",&b); ， %c把终端中的 所有的输入都作为有效的输入
+        因此 回车 被作为有效的输入给了b ， b的值是10 ， 就是回车键 
+        之后程序执行了 printf("b = %d\n", b);
+        因此 10 被打印出来了
+    */
+    return 0;
+}
